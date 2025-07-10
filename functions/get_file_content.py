cdfee96db8d4092628fac3f9c04d6090b986fa9d
hachaeva <hachaeva@hotmail.com>
2025-07-10 18:44:52 -0400

    added file reader

---
+++ functions/get_file_content.py
@@ -0,0 +1,18 @@
+import os
+
+MAX_CHARS = 10000
+
+def get_file_content(working_directory, file_path):
+    try:
+        relative_path = os.path.join(working_directory, file_path)
+        if not os.path.isfile(relative_path):
+            return f'Error: File not found or is not a regular file: "{file_path}"'
+        if not os.path.abspath(relative_path).startswith(os.path.abspath(working_directory)):
+            return f'Error: Cannot read "{file_path}" as it is outside the permitted working directory'
+        with open(relative_path, "r") as f:
+            file_content_string = f.read(MAX_CHARS)
+            if len(file_content_string) == MAX_CHARS:
+                file_content_string = file_content_string + f'...File "{file_path}" truncated at {MAX_CHARS} characters'
+        return file_content_string
+    except Exception as e:
+        return e
--- tests.py
+++ tests.py
@@ -1,7 +1,16 @@
 from functions.get_files_info import get_files_info
+from functions.get_file_content import get_file_content
+from functions.write_file import write_file
 
 if __name__ == "__main__":
-    print (f"{get_files_info("calculator",".")}")
-    print (f"{get_files_info("calculator","pkg")}")
-    print (f"{get_files_info("calculator","/bin")}")
-    print (f"{get_files_info("calculator","../")}")
+    #print (f"{get_files_info("calculator",".")}")
+    #print (f"{get_files_info("calculator","pkg")}")
+    #print (f"{get_files_info("calculator","/bin")}")
+    #print (f"{get_files_info("calculator","../")}")
+    #print (f"{get_file_content("calculator", "lorem.txt")}")
+    #print (f"{get_file_content("calculator", "main.py")}")
+    #print (f"{get_file_content("calculator", "pkg/calculator.py")}")
+    #print (f"{get_file_content("calculator", "/bin/cat")}")
+    #print (f"{write_file("calculator", "lorem.txt", "wait, this isn't lorem ipsum")}")
+    #print (f"{write_file("calculator", "pkg/morelorem.txt", "lorem ipsum dolor sit amet")}")
+    #print (f"{write_file("calculator", "/tmp/temp.txt", "this should not be allowed")}")
